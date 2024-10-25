d5c1b926d136724ba5e5977c05604742d7bd2fcf
R Hyde <rod.hyde@gmail.com>
2024-10-25 23:27:40 +0100

    lbavm012: init gp

--- source/crt0.s
+++ source/crt0.s
@@ -6,7 +6,11 @@ _start:
     .cfi_startproc
     .cfi_undefined ra
 
-    # TODO: turn off relaxations, set up the global pointer, re-enable relaxations.
+    # Set the global pointer.
+.option push
+.option norelax
+    la gp, __global_pointer$
+.option pop
 
     # TODO: set the stack pointer.
 
@@ -14,14 +18,13 @@ _start:
     call    init_vma
 
     # Call main().
-    li		a0,0        # a0 = argc = 0
-    li		a1,0		# a1 = argv = NULL
-    li		a2,0		# a2 = envp = NULL
+    li		a0, 0       # a0 = argc = 0
+    li		a1, 0		# a1 = argv = NULL
+    li		a2, 0		# a2 = envp = NULL
     call    main
 
     # Exit.
-    li      a0,0        # a0 = exit code
-    li      a7,0        # a7 = syscall number (0 is exit)
+    li      a7, 0       # a7 = syscall number (0 is exit)
     ecall               # do a syscall. There's no coming back from this one.
 
     .cfi_endproc
