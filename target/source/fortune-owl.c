df607547a762994845b4a9ba7bbf9ff213050cc1
R Hyde <rod.hyde@gmail.com>
2024-10-27 12:11:09 +0000

    lbavm012: extract syscalls to header

--- target/source/fortune-owl.c
+++ target/source/fortune-owl.c
@@ -1,71 +1,7 @@
+#include "syscalls.h"
+
 #include <stddef.h>
 #include <stdint.h>
-
-// Notes on syscall invocation and inline assembly.
-//
-// See: https://gcc.gnu.org/onlinedocs/gcc/Local-Register-Variables.html
-//
-// See: https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html
-//      https://gcc.gnu.org/onlinedocs/gcc/Simple-Constraints.html
-//      https://gcc.gnu.org/onlinedocs/gcc/Modifiers.html
-// and: https://www.felixcloutier.com/documents/gcc-asm.html
-//
-
-static inline void print_fib(uint32_t i, uint32_t result)
-{
-    // Define local register variables and associate them with specific RISC-V registers.
-    register uint32_t a7 __asm__("a7") = 1;      // a7 = syscall number
-    register uint32_t a0 __asm__("a0") = i;      // a0 = first argument to syscall
-    register uint32_t a1 __asm__("a1") = result; // a1 = second argument to syscall
-
-    // Invoke the `print_fib` syscall.
-    __asm__ __volatile__(
-            "ecall /* syscall(print_fib) */" // assembler template: ecall (with a comment)
-            : "+r"(a0)                       // output operands: reads and writes a0
-            : "r"(a7), "r"(a0), "r"(a1)      // input operands: reads a7, a0, a1
-            : "memory");                     // clobbers: this instruction may modify memory
-}
-
-static inline void puts(const char* s)
-{
-    // Define local register variables and associate them with specific RISC-V registers.
-    register uint32_t a7 __asm__("a7") = 2;              // a7 = syscall number
-    register uint32_t a0 __asm__("a0") = (uintptr_t)(s); // a0 = first argument to syscall
-
-    // Invoke the `puts` syscall.
-    __asm__ __volatile__("ecall /* syscall(puts) */" // assembler template: ecall (with a comment)
-                         : "+r"(a0)                  // output operands: reads and writes a0
-                         : "r"(a7), "r"(a0)          // input operands: reads a7, a0
-                         : "memory");                // clobbers: this instruction may modify memory
-}
-
-static inline void randomize(void)
-{
-    // Define local register variables and associate them with specific RISC-V registers.
-    register uint32_t a7 __asm__("a7") = 3; // a7 = syscall number
-
-    // Invoke the `randomize` syscall.
-    __asm__ __volatile__(
-            "ecall /* syscall(randomize) */" // assembler template: ecall (with a comment)
-            :                                // output operands: none
-            : "r"(a7)                        // input operands: reads a7
-            : "memory");                     // clobbers: this instruction may modify memory
-}
-
-static inline uint32_t random(uint32_t n)
-{
-    // Define local register variables and associate them with specific RISC-V registers.
-    register uint32_t a7 __asm__("a7") = 4; // a7 = syscall number
-    register uint32_t a0 __asm__("a0") = n; // a0 = first argument to syscall
-
-    // Invoke the `random` syscall.
-    __asm__ __volatile__("ecall /* syscall(random) */" // assembler template: ecall (with a comment)
-                         : "+r"(a0)                    // output operands: reads and writes a0
-                         : "r"(a7), "r"(a0)            // input operands: reads a7, a0
-                         : "memory"); // clobbers: this instruction may modify memory
-
-    return a0;
-}
 
 // Read only data. The strings, and the array itself, go into the `.rodata` section.
 static char* aphorisms[] = {
