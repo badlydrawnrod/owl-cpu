1d6a0f83229a33655a401b443fddf94946ac672e
R Hyde <rod.hyde@gmail.com>
2024-11-03 22:22:59 +0000

    elf-loader: add more functionality

--- source/elf_loader.cpp
+++ source/elf_loader.cpp
@@ -1,7 +1,9 @@
 #include <cstdint>
+#include <filesystem>
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <system_error>
 
 // ELF format references:
 // https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
@@ -25,10 +27,6 @@
 #define PF_X 1
 #define PF_W 2
 #define PF_R 4
-
-#define ST_TEXT (PF_R | PF_X)
-#define ST_RODATA PF_R
-#define ST_DATA (PF_R | PF_W)
 
 using Elf32_Addr = uint32_t;
 using Elf32_Off = uint32_t;
@@ -66,7 +64,7 @@ struct Elf32_Phdr
 };
 
 // Section header (Shdr).
-typedef struct Elf32_Shdr
+struct Elf32_Shdr
 {
     uint32_t sh_name;
     uint32_t sh_type;
@@ -78,123 +76,279 @@ typedef struct Elf32_Shdr
     uint32_t sh_info;
     uint32_t sh_addralign;
     uint32_t sh_entsize;
-} Elf32_Shdr;
+};
 
-void ReadElf(std::ifstream& ifs, uint32_t fileSize)
+struct Elf32_Headers
+{
+    Elf32_Ehdr ehdr;
+    std::vector<Elf32_Phdr> phdrs;
+    std::vector<Elf32_Shdr> shdrs;
+    std::vector<std::string> snames;
+};
+
+enum class elf_errc
+{
+    ER_OK = 0,           // The ELF file was loaded successfully.
+    ER_BAD_ELF,          // The ELF file is badly formatted in some way.
+    ER_INVALID_ARGUMENT, // The caller passed a bad argument.
+    ER_IO_FAILED,        // An I/O operation failed while reading the ELF file.
+    ER_NOT_SUPPORTED,    // The loader doesn't support some aspect of the ELF file, e.g., it isn't
+                         // RISC-V.
+};
+
+std::string to_string(elf_errc err)
+{
+    using enum elf_errc;
+    switch (err)
+    {
+    case ER_OK:
+        return "ok";
+    case ER_BAD_ELF:
+        return "badly formatted ELF file";
+    case ER_INVALID_ARGUMENT:
+        return "invalid argument";
+    case ER_IO_FAILED:
+        return "i/o failed";
+    case ER_NOT_SUPPORTED:
+        return "not supported";
+    }
+    return "unknown";
+}
+
+Elf32_Ehdr ReadElfHeader(std::ifstream& ifs, uint32_t fileSize, elf_errc& err)
 {
     // Read the ELF header.
     Elf32_Ehdr ehdr{};
-    ifs.read(reinterpret_cast<char*>(&ehdr), sizeof(ehdr));
 
-    // --- Check the e_ident portion of the header.
+    ifs.read(reinterpret_cast<char*>(&ehdr), sizeof(ehdr));
+    if (!ifs)
+    {
+        err = elf_errc::ER_IO_FAILED;
+        return {};
+    }
 
     // Check the magic number.
     const auto& e_ident = ehdr.e_ident;
     if (e_ident[EI_MAG0] != '\x7f' || e_ident[EI_MAG1] != 'E' || e_ident[EI_MAG2] != 'L'
         || e_ident[EI_MAG3] != 'F')
     {
-        std::cout << "Bad magic\n";
-        return;
+        err = elf_errc::ER_BAD_ELF;
+        return {};
     }
 
     // Check that it is 32-bit.
     if (e_ident[EI_CLASS] != 1)
     {
-        std::cout << "Unsupported: Not 32-bit ELF\n";
-        return;
+        err = elf_errc::ER_NOT_SUPPORTED;
+        return {};
     }
 
     // Check that it is two's complement, little-endian.
     if (e_ident[EI_DATA] != 1)
     {
-        std::cout << "Unsupported: Not two's complement, little-endian\n";
-        return;
+        err = elf_errc::ER_NOT_SUPPORTED;
+        return {};
     }
 
     // Check the ident version.
     if (e_ident[EI_VERSION] != 1)
     {
-        std::cout << "Unsupported: Unknown eident version\n";
-        return;
+        err = elf_errc::ER_NOT_SUPPORTED;
+        return {};
     }
-
-    // --- Check that it's a RISC-V executable.
 
     // Check that it is an executable.
     if (ehdr.e_type != ET_EXEC)
     {
-        std::cout << "Unsupported: Not an executable\n";
-        return;
+        err = elf_errc::ER_NOT_SUPPORTED;
+        return {};
     }
 
     // Check that it is for RISC-V.
     if (ehdr.e_machine != EM_RISCV)
     {
-        std::cout << "Unsupported: Not for a RISC-V machine\n";
-        return;
+        err = elf_errc::ER_NOT_SUPPORTED;
+        return {};
     }
 
     // Check the version.
     if (ehdr.e_version != 1)
     {
-        std::cout << "Unsupported: Unknown ehdr version\n";
-        return;
+        err = elf_errc::ER_NOT_SUPPORTED;
+        return {};
     }
-
-    // --- Sanity check the program header table.
 
     // Check that the size of a program header entry is what we expect it to be.
     if (ehdr.e_phentsize != sizeof(Elf32_Phdr))
     {
-        std::cout << "Unsupported: Program header size mismatch\n";
-        return;
+        err = elf_errc::ER_BAD_ELF;
+        return {};
     }
 
     // Check that the program header table is beyond the ELF header and within the file.
     // TODO: overflow, overlap.
     if (ehdr.e_phoff < sizeof(ehdr) || ehdr.e_phoff + ehdr.e_phentsize * ehdr.e_phnum > fileSize)
     {
-        std::cout << "Unsupported: Invalid program header table location\n";
-        return;
+        err = elf_errc::ER_BAD_ELF;
+        return {};
     }
-
-    // --- Sanity check the section header table.
 
     // Check that the size of a section header entry is what we expect it to be.
     if (ehdr.e_shentsize != sizeof(Elf32_Shdr))
     {
-        std::cout << "Unsupported: Section header size mismatch\n";
-        return;
+        err = elf_errc::ER_BAD_ELF;
+        return {};
     }
 
     // Check that the section header table is beyond the ELF header and within the file.
     // TODO: overflow, overlap.
     if (ehdr.e_shoff < sizeof(ehdr) || ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shnum > fileSize)
     {
-        std::cout << "Unsupported: Invalid section header table location\n";
-        return;
+        err = elf_errc::ER_BAD_ELF;
+        return {};
     }
 
-    // At this point we know that this is an ELF file for a 32-bit RISC-V executable and its program
-    // header table and section header table are at least within the file.
+    // Check that the string section is in bounds.
+    if (ehdr.e_shstrndx >= ehdr.e_shnum)
+    {
+        err = elf_errc::ER_BAD_ELF;
+        return {};
+    }
 
+    return ehdr;
+}
+
+Elf32_Phdr ReadProgramHeader(std::ifstream& ifs, uint32_t fileSize, elf_errc& err)
+{
+    // Read a program header.
+    Elf32_Phdr phdr{};
+    ifs.read(reinterpret_cast<char*>(&phdr), sizeof(phdr));
+    if (!ifs)
+    {
+        err = elf_errc::ER_IO_FAILED;
+        return {};
+    }
+
+    // The file size may not be larger than the memory size.
+    if (phdr.p_filesz > phdr.p_memsz)
+    {
+        err = elf_errc::ER_BAD_ELF;
+        return {};
+    }
+
+    // TODO: other validation.
+
+    return phdr;
+}
+
+Elf32_Shdr ReadSectionHeader(std::ifstream& ifs, uint32_t fileSize, elf_errc& err)
+{
+    // Read a section header.
+    Elf32_Shdr shdr{};
+    ifs.read(reinterpret_cast<char*>(&shdr), sizeof(shdr));
+    if (!ifs)
+    {
+        err = elf_errc::ER_IO_FAILED;
+        return {};
+    }
+
+    // TODO: other validation.
+
+    return shdr;
+}
+
+Elf32_Headers ReadElf(std::ifstream& ifs, uint32_t fileSize, elf_errc& err)
+{
+    // Read the ELF header.
+    Elf32_Ehdr ehdr = ReadElfHeader(ifs, fileSize, err);
+    if (err != elf_errc::ER_OK)
+    {
+        return {};
+    }
+
+    // Read the program headers.
+    std::vector<Elf32_Phdr> phdrs;
+    phdrs.reserve(ehdr.e_phnum);
     for (uint16_t i = 0; i < ehdr.e_phnum; i++)
     {
         // Go to the program header's entry in the program header table.
         ifs.seekg(ehdr.e_phoff + i * ehdr.e_phentsize);
+        if (!ifs)
+        {
+            err = elf_errc::ER_IO_FAILED;
+            return {};
+        }
 
-        // Read the program header.
-        Elf32_Phdr phdr{};
-        ifs.read(reinterpret_cast<char*>(&phdr), sizeof(phdr));
+        Elf32_Phdr phdr = ReadProgramHeader(ifs, fileSize, err);
+        if (err != elf_errc::ER_OK)
+        {
+            return {};
+        }
+        phdrs.push_back(phdr);
+    }
 
+    // Read the section headers.
+    std::vector<Elf32_Shdr> shdrs;
+    shdrs.reserve(ehdr.e_shnum);
+    for (uint16_t i = 0; i < ehdr.e_shnum; i++)
+    {
+        // Go to the section header's entry in the program header table.
+        ifs.seekg(ehdr.e_shoff + i * ehdr.e_shentsize);
+        if (!ifs)
+        {
+            err = elf_errc::ER_IO_FAILED;
+            return {};
+        }
+
+        Elf32_Shdr shdr = ReadSectionHeader(ifs, fileSize, err);
+        if (err != elf_errc::ER_OK)
+        {
+            return {};
+        }
+        shdrs.push_back(shdr);
+    }
+
+    // Read the section names.
+    std::vector<std::string> snames;
+    shdrs.reserve(ehdr.e_shnum);
+    const Elf32_Shdr& stringSection = shdrs[ehdr.e_shstrndx];
+    const auto offset = stringSection.sh_offset;
+    for (auto& shdr : shdrs)
+    {
+        ifs.seekg(offset + shdr.sh_name);
+        std::string name;
+        std::getline(ifs, name, '\0');
+        snames.push_back(name);
+    }
+    
+    Elf32_Headers headers{.ehdr = ehdr, .phdrs = phdrs, .shdrs = shdrs, .snames = snames};
+    return headers;
+}
+
+int main()
+{
+    const char path[] = "../../target/out/a.out";
+    std::ifstream elfStream(path, std::ios::binary | std::ios::ate);
+    if (!elfStream)
+    {
+        std::cout << "Couldn't open: " << path << '\n';
+        return 1;
+    }
+    auto fileSize = elfStream.tellg();
+    elfStream.seekg(0);
+
+    elf_errc err{};
+    Elf32_Headers headers = ReadElf(elfStream, fileSize, err);
+    if (err != elf_errc::ER_OK)
+    {
+        std::cout << to_string(err) << '\n';
+        return 1;
+    }
+
+    for (const auto& phdr : headers.phdrs)
+    {
         if (phdr.p_type == PT_LOAD)
         {
-            if (phdr.p_filesz > phdr.p_memsz)
-            {
-                // The file size may not be larget than the memory size.
-                continue;
-            }
-
             // Classify it based on flags and sizes.
             std::string type{};
             const auto& flags = phdr.p_flags;
@@ -219,28 +373,14 @@ void ReadElf(std::ifstream& ifs, uint32_t fileSize)
                 type = "UNKNOWN";
             }
 
-            std::cout << std::format("Found loadable {} segment at p_offset = {:08x}\n", type, phdr.p_offset);
+            std::cout << std::format("Found loadable {} segment at p_offset = {:08x}\n", type,
+                                     phdr.p_offset);
             std::cout << std::format("\t p_paddr = {:08x}\n", phdr.p_paddr);
             std::cout << std::format("\t p_vaddr = {:08x}\n", phdr.p_vaddr);
             std::cout << std::format("\tp_filesz = {:08x}\n", phdr.p_filesz);
             std::cout << std::format("\t p_memsz = {:08x}\n", phdr.p_memsz);
         }
     }
-}
-
-int main()
-{
-    const char path[] = "../../target/out/a.out";
-    std::ifstream elfStream(path, std::ios::binary | std::ios::ate);
-    if (!elfStream)
-    {
-        std::cout << "Couldn't open: " << path << '\n';
-        return 1;
-    }
-    auto fileSize = elfStream.tellg();
-    elfStream.seekg(0);
-
-    ReadElf(elfStream, fileSize);
 
     return 0;
 }
