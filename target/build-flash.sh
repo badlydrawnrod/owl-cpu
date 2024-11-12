e09d4efdeb8dfe80db7322730b5ae7d1f3235c9b
R Hyde <rod.hyde@gmail.com>
2024-11-12 23:29:07 +0000

    elf-loader: double-check on Linux

--- target/build-flash.sh
+++ target/build-flash.sh
@@ -1,2 +1,1 @@
 clang -DUSE_FLASH -Os --target=riscv32 -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -nodefaultlibs ./source/crt0.S ./source/fortune-owl.c -fuse-ld=lld "-Wl,--gc-sections" -T./source/memory_flash.ld -T./source/sections.ld -o out/a.out
-llvm-objcopy out/a.out -O binary out/a.bin -j .init -j .text -j .rodata -j .sdata -j .data
