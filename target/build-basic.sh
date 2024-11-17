e0e1c20ca4bae1ea7a3c126a238817b810d5a447
R Hyde <rod.hyde@gmail.com>
2024-11-17 11:34:03 +0000

    lbavm-012: more robust bash

--- target/build-basic.sh
+++ target/build-basic.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/bash
+
 if [[ "$#" -ne 1 ]]; then
     echo "Usage: $(basename $0) <filename>" >&2
     exit 2
 fi
 
+pushd $(dirname $0) > /dev/null
+
 source=$1
 clang -Os --target=riscv32 -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -nodefaultlibs ./source/crt0.S ./source/$source -fuse-ld=lld "-Wl,--gc-sections" -T./source/basic.ld -o out/a.out
+
+popd > /dev/null
--- target/build-flash.sh
+++ target/build-flash.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/bash
+
 if [[ "$#" -ne 1 ]]; then
     echo "Usage: $(basename $0) <filename>" >&2
     exit 2
 fi
 
+pushd $(dirname $0) > /dev/null
+
 source=$1
 clang -DUSE_FLASH -Os --target=riscv32 -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -nodefaultlibs ./source/crt0.S ./source/$source -fuse-ld=lld "-Wl,--gc-sections" -T./source/memory_flash.ld -T./source/sections.ld -o out/a.out
+
+popd > /dev/null
